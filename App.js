34ecd4e2feee69ac7f9b38f479514e96ca947af0
unknown <krishnacguda16@gmail.com>
2022-07-13 00:04:15 -0700

    changes in app.js file

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NativeBaseProvider, extendTheme} from 'native-base';
+import { NativeBaseProvider, extendTheme, Text, Button} from 'native-base';
 import AppStack from './src/navigation/appStack/index';
 import store from './src/redux/store'
 import { Provider } from 'react-redux';
@@ -10,19 +10,32 @@ import GetLocation from 'react-native-get-location'
 
 const App = () => {
 
+  const [locationText, setLocationText] = React.useState("")
+
   React.useEffect(() => {    
+    getCurretnLocation()
+  },[])
+
+
+
+
+  const getCurretnLocation = () =>{
     GetLocation.getCurrentPosition({
       enableHighAccuracy: true,
       timeout: 15000,
     })
     .then(location => {
       console.log(location);
+      setLocationText(location)
     })
     .catch(error => {
       const { code, message } = error;
       console.warn(code, message);
     })
-  },[])
+  }
+
+
+
 
   return (
     <Provider store={store} theme={theme}>
@@ -31,6 +44,8 @@ const App = () => {
           backgroundColor="#eee"
       />
       <NativeBaseProvider>
+          <Text>{locationText ? `Longitude: ${locationText.longitude}, Latitude: ${locationText.latitude}` : null}</Text>
+          <Button onPress={() => getCurretnLocation()}>Get Current Location</Button>
           <AppStack />
       </NativeBaseProvider>
     </Provider>
